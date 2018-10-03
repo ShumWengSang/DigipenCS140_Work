1fc89e9f15b31b00b3bde5e30b02388fd587aebb
ballwallcallall <ballwallcallall@gmail.com>
2018-10-03 16:38:33 -0700

    Modifised function added comments.
    need more comments.

--- 2018/CS120/Assignments/pi.c
+++ 2018/CS120/Assignments/pi.c
@@ -1,32 +1,39 @@
 #include <math.h> /*sqrt */
-#include <stdio.h>
+
 #define RADIUS 2.0
-#define TWO 2.0
+#define TWO_FACTOR 2.0
 #define NUMERATOR 1.0
+#define POSITIVE 1
+#define NEGATIVE -1
 double circle_pi(int rectangles)
 {
   int i; /*loop variable*/
 
-  double halfwidth;
-  double totalArea;
+  double halfwidth;  /* Half of the width a rectangle.   */
+  double totalArea;  /* Holds the total area for return. */
   
+  /*Set to zero. */
   totalArea = 0;
-  halfwidth = (RADIUS / rectangles ) / TWO;
+  
+  /* Width = (radius / number of rectangles). Divide by two to get half of it. */
+  halfwidth = (RADIUS / rectangles ) / TWO_FACTOR;  
   
   for(i = 1; i <= rectangles; i++)
   { 
-    double length;
-    double height;
+    double length; /* The calculated length, used to find height.       */
+    double height; /* The height, used to find the area of the new rec. */
+    
     /* 
      * Calculate the midpoint of the current rectangle by
      * (width * previous iteration number) + halfwidth
      */
-    length = (halfwidth * TWO) * (i - 1) + halfwidth;
+    length = (halfwidth * TWO_FACTOR) * (i - 1) + halfwidth;
 
     /* Calc height by distance formula. */
     height = sqrt((RADIUS * RADIUS) - (length * length));
-    /* Find new area and add to total, using base * height */
-    totalArea =  totalArea + (halfwidth * TWO * height);
+    
+    /* Find new area and add to total, using base * height. */
+    totalArea =  totalArea + (halfwidth * TWO_FACTOR * height);
   }
   
   return totalArea;
@@ -34,23 +41,26 @@ double circle_pi(int rectangles)
 
 double leibniz_pi(int iterations)
 {
-  double totalOutcome = 0;
-  int sign;
-  /* exits loop when iterations is 0. */
-  while(--iterations)
+  double totalOutcome = 0;  /* The result of adding all the numbers. */
+  
+  /* Exits loop when iteration is 0. */
+  while(iterations)
   {
-    int denominator = iterations + 2;
-    if(iterations % 2 == 1)
-    {
-      /*take away*/
-      sign = -1;
-    }
-    else
-    {
-      /*add to*/
-      sign = 1;
-    }
-    totalOutcome = totalOutcome + (sign * (NUMERATOR / denominator));
+    int sign; /* The sign of the number. */
+    
+    /* Denominator is determined by pattern, (n * 2) - 1 where n is the current term number. */
+    int denominator = (iterations * 2) - 1; 
+    
+    /* Determine the sign of the number based on whether iteration is even or odd. */
+    sign = (iterations % 2) == 0 ? NEGATIVE : POSITIVE;
+    
+    /*Calculate the inverse of denominator. Attach the sign to it. Add to totalOutcome. */
+    totalOutcome += (sign * (NUMERATOR / denominator));
+    
+    /*Move to next number. */
+    iterations--;
   }
-  return 4 * (1 + totalOutcome);
+  
+  /* totalOutcome gives (pi / 4), so we times 4 to give pi. */
+  return 4 * (totalOutcome);
 }
